7dd79c7eaff301a4ab042b7f16a5e7967e73acc1
Analia Mok <axm2580@g.rit.edu>
2018-08-05 15:00:25 -0400

    Created posts and category table migrations. Adjusted users table.

---
+++ geek-news/database/migrations/2018_08_05_182022_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->unsignedInteger('id');
+            $table->unsignedInteger('wp_post_id');
+            $table->boolean('is_featured')->default(0);
+            $table->integer('featured_image')->nullable();
+            $table->enum('post_status', ['draft', 'auto-draft', 'pending', 'publish'])->default('publish');
+            $table->string('post_title');
+            $table->text('excerpt')->nullable();
+            $table->longText('content')->nullable();
+
+            // Adds nullable created_at and updated_at
+            $table->timestamps();
+            $table->timestamp('published_at');
+
+            // Future todo: Create foreign key references to these ids
+            $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedInteger('category_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- geek-news/database/migrations/2018_08_05_185604_create_category_table.php
+++ geek-news/database/migrations/2018_08_05_185604_create_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('category', function (Blueprint $table) {
+            $table->unsignedInteger('id');
+            $table->unsignedInteger('wp_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->string('slug');
-            $table->string('avatar_image')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category');
     }
 }
